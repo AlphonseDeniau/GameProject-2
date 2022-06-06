512b756f839cec223d19ee3a4979ae6cd6182046
Quentin Maillard <quentin.maillard@epitech.eu>
2022-06-06 16:05:35 +0900

    launching dungeon with team functional

--- Dungeons_of_KHAL/Assets/Scripts/Village/LaunchingExplo.cs
+++ Dungeons_of_KHAL/Assets/Scripts/Village/LaunchingExplo.cs
@@ -12,10 +12,18 @@ public class LaunchingExplo : MonoBehaviour
     public Button Warrior_BTN;
     public Button Wizard_BTN;
 
-    var selected_character = new List<string>();
+    List<CharacterObject> availableCharacters;
+    CharacterManager team;
+    int maxMembers = 4;
+    List<int> tmpIndexBackup;
+    List<CharacterObject> objRef = new List<CharacterObject>();
 
     void Start()
     {
+        availableCharacters = GameManager.Instance.GlobalCharacterManager.Allies;
+        team = GameManager.Instance.ExplorationCharacterManager;
+        tmpIndexBackup = new List<int>();
+
         Priest_BTN.onClick.AddListener(priestManagement);
         Ranger_BTN.onClick.AddListener(rangerManagement);
         Templar_BTN.onClick.AddListener(templarManagement);
@@ -24,33 +32,62 @@ public class LaunchingExplo : MonoBehaviour
         Wizard_BTN.onClick.AddListener(wizardManagement);
     }
 
+    public void MoveCharacters() {
+        foreach (CharacterObject obj in objRef) {
+            obj.transform.SetParent(team.transform);
+            GameManager.Instance.GlobalCharacterManager.RemoveCharacter(obj, Character.ETeam.Ally);
+        }
+    }
+
+    void addCharacterToTeam(int index) {
+        if (tmpIndexBackup.IndexOf(index) != -1)
+            return;
+        if (team.Allies.Count >= maxMembers) {
+            team.RemoveCharacter(availableCharacters[tmpIndexBackup[0]], Character.ETeam.Ally);
+            tmpIndexBackup.RemoveAt(0);
+            foreach (CharacterObject obj in team.Allies) {
+                obj.Data.Position -= 1;
+            }
+        }
+        availableCharacters[index].Data.Position = team.Allies.Count;
+        team.AddCharacter(availableCharacters[index], Character.ETeam.Ally);
+        tmpIndexBackup.Add(index);
+        objRef.Add(availableCharacters[index]);
+    }
+
     void priestManagement()
     {
-        Debug.Log("Priest !!!");
+        addCharacterToTeam(0);
+        Priest_BTN.interactable = false;
     }
 
     void rangerManagement()
     {
-        Debug.Log("Ranger !!!");
+        addCharacterToTeam(1);
+        Priest_BTN.interactable = false;
     }
 
     void templarManagement()
     {
-        Debug.Log("Templar !!!");
+        addCharacterToTeam(2);
+        Priest_BTN.interactable = false;
     }
 
     void thiefManagement()
     {
-        Debug.Log("Thief !!!");
+        addCharacterToTeam(3);
+        Priest_BTN.interactable = false;
     }
 
     void warriorManagement()
     {
-        Debug.Log("Warrior !!!");
+        addCharacterToTeam(4);
+        Priest_BTN.interactable = false;
     }
 
     void wizardManagement()
     {
-        Debug.Log("Wizard !!!");
+        addCharacterToTeam(5);
+        Priest_BTN.interactable = false;
     }
 }
