d137d13766fe1588433c95b114385864b373945d
Auguste <auguste.alexandre@opencellsoft.com>
2022-06-09 17:00:10 +0900

    [ADD]: You can now remove character from your selection before launching the expedition, feature added

--- Dungeons_of_KHAL/Assets/Scripts/Village/LaunchingExplo.cs
+++ Dungeons_of_KHAL/Assets/Scripts/Village/LaunchingExplo.cs
@@ -6,17 +6,24 @@ using UnityEngine.UI;
 public class LaunchingExplo : MonoBehaviour
 {
     public Button Priest_BTN;
+    public Button Priest_minus_BTN;
     public Button Ranger_BTN;
+    public Button Ranger_minus_BTN;
     public Button Templar_BTN;
+    public Button Templar_minus_BTN;
     public Button Thief_BTN;
+    public Button Thief_minus_BTN;
     public Button Warrior_BTN;
+    public Button Warrior_minus_BTN;
     public Button Wizard_BTN;
+    public Button Wizard_minus_BTN;
 
     List<CharacterObject> availableCharacters;
     CharacterManager team;
     int maxMembers = 4;
     List<int> tmpIndexBackup;
     List<CharacterObject> objRef = new List<CharacterObject>();
+    Character.ETeam eTeam;
 
     void Start()
     {
@@ -25,11 +32,17 @@ public class LaunchingExplo : MonoBehaviour
         tmpIndexBackup = new List<int>();
 
         Priest_BTN.onClick.AddListener(priestManagement);
+        Priest_minus_BTN.onClick.AddListener(priestMinusManagement);
         Ranger_BTN.onClick.AddListener(rangerManagement);
+        Ranger_minus_BTN.onClick.AddListener(rangerMinusManagement);
         Templar_BTN.onClick.AddListener(templarManagement);
+        Templar_minus_BTN.onClick.AddListener(templarMinusManagement);
         Thief_BTN.onClick.AddListener(thiefManagement);
+        Thief_minus_BTN.onClick.AddListener(thiefMinusManagement);
         Warrior_BTN.onClick.AddListener(warriorManagement);
+        Warrior_minus_BTN.onClick.AddListener(warriorMinusManagement);
         Wizard_BTN.onClick.AddListener(wizardManagement);
+        Wizard_minus_BTN.onClick.AddListener(wizardMinusManagement);
     }
 
     public void MoveCharacters() {
@@ -55,39 +68,108 @@ public class LaunchingExplo : MonoBehaviour
         objRef.Add(availableCharacters[index]);
     }
 
+    void removeCharacterFromTeam(int index)
+    {
+        team = GameManager.Instance.ExplorationCharacterManager;
+        team.RemoveCharacter(objRef[index], eTeam);
+    }
+
     void priestManagement()
     {
         addCharacterToTeam(0);
-        Priest_BTN.interactable = false;
+        if (Priest_BTN.interactable == true && Priest_minus_BTN.interactable == false) {
+            Priest_BTN.interactable = false;
+            Priest_minus_BTN.interactable = true;
+        }
+
+    }
+
+    void priestMinusManagement()
+    {
+        removeCharacterFromTeam(0);
+        if (Priest_BTN.interactable == false && Priest_minus_BTN.interactable == true) {
+            Priest_BTN.interactable = true;
+            Priest_minus_BTN.interactable = false;
+        }
     }
 
     void rangerManagement()
     {
         addCharacterToTeam(1);
-        Priest_BTN.interactable = false;
+        if (Ranger_BTN.interactable == true && Ranger_minus_BTN.interactable == false)
+            Ranger_BTN.interactable = false;
+            Ranger_minus_BTN.interactable = true;
+    }
+
+    void rangerMinusManagement()
+    {
+        removeCharacterFromTeam(1);
+        if (Ranger_BTN.interactable == false && Ranger_minus_BTN.interactable == true)
+            Ranger_BTN.interactable = true;
+            Ranger_minus_BTN.interactable = false;
     }
 
     void templarManagement()
     {
         addCharacterToTeam(2);
-        Priest_BTN.interactable = false;
+        if (Templar_BTN.interactable == true && Templar_minus_BTN.interactable == false)
+            Templar_BTN.interactable = false;
+            Templar_minus_BTN.interactable = true;
+    }
+    
+    void templarMinusManagement()
+    {
+        removeCharacterFromTeam(2);
+        if (Templar_BTN.interactable == false && Templar_minus_BTN.interactable == true)
+            Templar_BTN.interactable = true;
+            Templar_minus_BTN.interactable = false;
     }
 
     void thiefManagement()
     {
         addCharacterToTeam(3);
-        Priest_BTN.interactable = false;
+        if (Thief_BTN.interactable == true && Thief_minus_BTN.interactable == false)
+            Thief_BTN.interactable = false;
+            Thief_minus_BTN.interactable = true;
+    }
+
+    void thiefMinusManagement()
+    {
+        removeCharacterFromTeam(3);
+        if (Thief_BTN.interactable == false && Thief_minus_BTN.interactable == true)
+            Thief_BTN.interactable = true;
+            Thief_minus_BTN.interactable = false;
     }
 
     void warriorManagement()
     {
         addCharacterToTeam(4);
-        Priest_BTN.interactable = false;
+        if (Warrior_BTN.interactable == true && Warrior_minus_BTN.interactable == false)
+            Warrior_BTN.interactable = false;
+            Warrior_minus_BTN.interactable = true;
+    }
+
+    void warriorMinusManagement()
+    {
+        removeCharacterFromTeam(4);
+        if (Warrior_BTN.interactable == false && Warrior_minus_BTN.interactable == true)
+            Warrior_BTN.interactable = true;
+            Warrior_minus_BTN.interactable = false;
     }
 
     void wizardManagement()
     {
         addCharacterToTeam(5);
-        Priest_BTN.interactable = false;
+        if (Wizard_BTN.interactable == true && Wizard_minus_BTN.interactable == false)
+            Wizard_BTN.interactable = false;
+            Wizard_minus_BTN.interactable = true;
+    }
+
+    void wizardMinusManagement()
+    {
+        removeCharacterFromTeam(5);
+        if (Wizard_BTN.interactable == false && Wizard_minus_BTN.interactable == true)
+            Wizard_BTN.interactable = true;
+            Wizard_minus_BTN.interactable = false;
     }
 }
